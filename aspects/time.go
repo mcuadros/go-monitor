be94fc1c4e9593914eb258a9692582a876a4d274
Máximo Cuadros Ortiz <mcuadros@gmail.com>
2015-02-09 08:00:52 +0100

    TimeAspect

---
+++ aspects/time.go
@@ -0,0 +1,34 @@
+package aspects
+
+import (
+	"time"
+)
+
+type TimeAspectData struct {
+	StartTime   time.Time
+	RequestTime time.Time
+}
+
+type TimeAspect struct {
+	StartTime  time.Time
+	ShowInRoot bool
+}
+
+func NewTimeAspect(inRoot bool) *TimeAspect {
+	return &TimeAspect{StartTime: time.Now(), ShowInRoot: inRoot}
+}
+
+func (a *TimeAspect) GetStats() interface{} {
+	return &TimeAspectData{
+		StartTime:   a.StartTime,
+		RequestTime: time.Now(),
+	}
+}
+
+func (a *TimeAspect) Name() string {
+	return "Time"
+}
+
+func (a *TimeAspect) InRoot() bool {
+	return a.ShowInRoot
+}
